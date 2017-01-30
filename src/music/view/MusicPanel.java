852c385334da1ecd66824ec31ef3f07374a3b501
mattpow35 <mpow2231@740s-pl-121.local>
2017-01-30 09:14:20 -0700

    created project and added ignore files

---
+++ src/music/view/MusicPanel.java
@@ -0,0 +1,27 @@
+package music.view;
+
+import javax.swing.*;
+
+public class MusicPanel extends JPanel 
+{
+	private JTextField rowField;
+	private JTextField columnField;
+	private JTextField inputField;
+	
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+
+}
